a6a0f3befb229b0e37ca65a9cdb93b3ef8e4c4d3
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2022-08-09 19:28:13 +0000

    QoL Changes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var copyButton = document.getElementById("copyUrlButton");
-
 function generate(){
 
   var userId = document.getElementById("inputUserId").value;
@@ -36,11 +34,14 @@ var url = `https://thumbnails.roproxy.com/v1/users/avatar?userIds=${userId}&size
 
 function copyUrl(){
   var userId = document.getElementById("inputUserId").value;
+  var userId = document.get
 var size = document.getElementById("size").value;
 var fileFormat = document.getElementById("fileFormat").value;
 var isCircular = document.getElementById("isCircular").value;
 var url = `https://thumbnails.roproxy.com/v1/users/avatar?userIds=${userId}&size=${size}&format=${fileFormat}&isCircular=${isCircular}`;
   var copyButton = document.getElementById("copyUrlButton")
+
+
 
   
   axios.get(url)
@@ -53,33 +54,6 @@ var url = `https://thumbnails.roproxy.com/v1/users/avatar?userIds=${userId}&size
 
     setTimeout(() => {
   copyButton.innerHTML = "Copy Image Url"
-}, 2000);
-    
-  })
-  .catch(error => {
-    console.log(error)
-  })
-  }
-
-function downloadThumbnail(){
-  var userId = document.getElementById("inputUserId").value;
-var size = document.getElementById("size").value;
-var fileFormat = document.getElementById("fileFormat").value;
-var isCircular = document.getElementById("isCircular").value;
-var url = `https://thumbnails.roproxy.com/v1/users/avatar?userIds=${userId}&size=${size}&format=${fileFormat}&isCircular=${isCircular}`;
-  var downloadButton = document.getElementById("downloadThumbnailButton");
-  var downloadLink = document.getElementById("downloadTrigger");
-
-  
-  axios.get(url)
-  .then(response => {
-    
-    imageUrl = response.data.data[0].imageUrl
-
-    downloadLink.d = imageUrl
-   
-    setTimeout(() => {
-  
 }, 2000);
     
   })
