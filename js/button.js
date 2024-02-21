bea60b83145c032a8917ed963cf018537c2183bc
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2024-02-21 05:31:48 +0000

    Fixed the entire website functionality + UI Improvements

--- js/button.js
+++ js/button.js
@@ -1,26 +1,26 @@
 const buttons = document.getElementsByTagName("button");
 
 function createRipple(event) {
-  const button = event.currentTarget;
+	const button = event.currentTarget;
 
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
+	const circle = document.createElement("span");
+	const diameter = Math.max(button.clientWidth, button.clientHeight);
+	const radius = diameter / 2;
 
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-  circle.classList.add("ripple");
+	circle.style.width = circle.style.height = `${diameter}px`;
+	circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+	circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+	circle.classList.add("ripple");
 
-  const ripple = button.getElementsByClassName("ripple")[0];
+	const ripple = button.getElementsByClassName("ripple")[0];
 
-  if (ripple) {
-    ripple.remove();
-  }
+	if (ripple) {
+		ripple.remove();
+	}
 
-  button.appendChild(circle);
+	button.appendChild(circle);
 }
 
 for (const button of buttons) {
-  button.addEventListener("click", createRipple);
+	button.addEventListener("click", createRipple);
 }
--- js/script.js
+++ js/script.js
@@ -1,58 +1,59 @@
-function generate(){
+const usernameInput = document.getElementById("inputUsername");
+const size = document.getElementById("size");
+const fileFormat = document.getElementById("fileFormat");
+const isCircular = document.getElementById("isCircular");
+const thumbnail = document.getElementById("thumbnail");
 
-	var userId = document.getElementById("inputUserId").value;
-var size = document.getElementById("size").value;
-var fileFormat = document.getElementById("fileFormat").value;
-var isCircular = document.getElementById("isCircular").value;
-var thumbnail = document.getElementById("thumbnail");
-var url = `https://roproxy.valiantwind.repl.co/thumbnails/v1/users/avatar?userIds=${userId}&size=${size}&format=${fileFormat}&isCircular=${isCircular}`;
-  var generateButton = document.getElementById("submit");
-	
-  axios.get(url)
-  .then(response => {
+async function fetchImageUrl(username, imageSize, imageFormat, circular) {
+	return new Promise((resolve, reject) => {
 
-thumbnail.src = response.data.data[0].imageUrl;
-		
-  })
-  .catch(error => {
-
-    generateButton.innerHTML = "Invalid ID!"
-
-    setTimeout(() => {
-  generateButton.innerHTML = "Generate Thumbnail"
-}, 3000);
-    console.log(error)
-    
-    
-  })
-  }
-
-
-
-var copyUrlButton = document.querySelector("copyUrlButton");
-
-if(copyUrlButton) {
-	var userId = document.getElementById("inputUserId").value;
-var size = document.getElementById("size").value;
-var fileFormat = document.getElementById("fileFormat").value;
-var isCircular = document.getElementById("isCircular").value;
-var thumbnail = document.getElementById("thumbnail");
-var url = `https://roproxy.valiantwind.repl.co/thumbnails/v1/users/avatar?userIds=${userId}&size=${size}&format=${fileFormat}&isCircular=${isCircular}`;
-	axios.get(url)
-  .then(response => {
-
-		const imageUrl = response.data.data[0].imageUrl;
-		console.log(imageUrl);
-
-		copyUrlButton.addEventListener("click", function(e) {
-			navigator.clipboard.writeText(imageUrl);	
-		});
-			
+	const config = {
+		url: `https://api.valiantwind.dev/v1/roblox/get-avatar-body-thumbnail?username=${username.value}&size=${imageSize.value}&format=${imageFormat.value}&isCircular=${circular.value}`,
+		method: 'get',
+	}
+		axios(config).then(response => {
+			resolve(response.data.data[0].imageUrl)
+		}).catch(error => {
+			reject(error)
+		})
 	})
-  .catch(error => {
+}
 
-    console.log(error)
-    
-    
-  });
+function generateThumbnail() {
+
+	const generateButton = document.getElementById("submit");
+
+	fetchImageUrl(usernameInput, size, fileFormat, isCircular)
+		.then(url => {
+			console.log(url)
+			thumbnail.src = url;
+		})
+		.catch(error => {
+			generateButton.innerHTML = "Something went wrong.";
+
+			setTimeout(() => {
+				generateButton.innerHTML = "Generate Thumbnail";
+			}, 3000)
+			console.log(error);
+		})
+}
+
+function copyImageUrl() {
+	const copyUrlButton = document.getElementById("copyUrlButton");
+
+	if (userIdInput && size && fileFormat && isCircular) {
+
+		fetchImageUrl(userIdInput, size, fileFormat, isCircular)
+			.then(url => {
+				console.log(url)
+				navigator.clipboard.writeText(url);
+
+				copyUrlButton.innerHTML = "Copied!"
+
+				setTimeout(() => {
+					copyUrlButton.innerHTML = "Copy Image URL";
+				}, 3000)
+			})
+
+	}
 }
